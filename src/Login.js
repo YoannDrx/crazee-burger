254397df38e21992e89965a42a656c092337dda4
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-03-25 17:39:41 +0100

    🔥 F01: Delete App.test.js

---
+++ src/Login.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export function Login() {
+    const [prenom, setPrenom] = useState("");
+
+    const handlechange = (e) => {
+        const newValue = e.target.value;
+        setPrenom(newValue);
+    };
+
+    const handleSubmit = () => {
+        alert(prenom)
+        setPrenom("")
+    }
+
+    return (
+        <div>
+            <h1>Bienvenue chez nous</h1>
+            <p>Connectez-vous</p>
+            <form onSubmit={handleSubmit}>
+                <input value={prenom} onChange={(e) => handlechange(e)} type="text" placeholder="Entrez votre prénom" required />
+                <p>Please fill this field</p>
+                <button>Accéder à votre espace</button>
+            </form>
+        </div>
+    );
+}

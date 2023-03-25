2b33855337f4dc76ce5b30a65002b3c91326035e
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-03-25 18:54:08 +0100

    🎨 F01: LOGIN > create LoginForm.js

---
+++ src/components/pages/login/LoginForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+    const [inputValue, setInputValue] = useState("");
+
+    const handlechange = (e) => {
+        const newValue = e.target.value;
+        setInputValue(newValue);
+    };
+
+    const handleSubmit = () => {
+        alert(`Bonjour ${inputValue}`);
+        setInputValue("");
+    };
+
+    return (
+        <form action="submit" onSubmit={handleSubmit}>
+            <h1>Bienvenue chez nous</h1>
+            <br />
+            <h2>Connectez-vous</h2>
+            <input value={inputValue} onChange={(e) => handlechange(e)} type="text" placeholder="Entrez votre prénom" required />
+            <p>Please fill this field</p>
+            <button>Accéder à votre espace</button>
+        </form>
+    );
+}

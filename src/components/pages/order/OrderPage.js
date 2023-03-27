af9ab4a2fc5bb0309c07280e386c4a4ab4c7346e
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-03-27 22:06:33 +0200

    ✨ F02: ORDER > create OrderPage.js

---
+++ src/components/pages/order/OrderPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function OrderPage() {
+    return (
+        <div>
+            <h1>Bonjour Bob</h1>
+            <button>Retourner à la page d'acceuil</button>
+        </div>
+    );
+}
+
+export default OrderPage;

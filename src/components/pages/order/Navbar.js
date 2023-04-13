02dac5308eb3d53f4cbfa7e9e65b46c218de8a31
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-13 21:36:36 +0200

    ♻️ F04: Navbar > create utils function

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import NavbarRightSide from "./NavbarRightSide";
 import Logo from "../../reusable-ui/Logo";
 import { theme } from "../../../theme";
+import { refreshPage } from "../../../utils/window";
 
 export default function Navbar({ username }) {
     return (
         <NavbarStyled className="navbar">
-            <Logo/>
+            <Logo onClick={refreshPage} className={"logo-order-page"}/>
            <NavbarRightSide username={username}/>
         </NavbarStyled>
     );
@@ -21,4 +22,7 @@ const NavbarStyled = styled.nav`
     border-top-left-radius: ${theme.borderRadius.extraRound};
     border-top-right-radius: ${theme.borderRadius.extraRound};
 
+.logo-order-page{
+    cursor: pointer;
+}
 `;

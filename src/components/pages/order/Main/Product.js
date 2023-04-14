cbfe70b3565286590aea45538003b4342733e98c
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-14 16:27:18 +0200

    💄 F05 : Product > add image layout

--- src/components/pages/order/Main/Product.js
+++ src/components/pages/order/Main/Product.js
@@ -7,8 +7,8 @@ export default function Product({ index, imageSource, title, price }) {
                 <img src={imageSource} alt="" />
             </div>
             <div className="info-text">
-                <div className="description">
                     <div className="title">{title}</div>
+                <div className="description">
                     <div className="price">{price}</div>
                     <button className="add-button">Ajouter</button>
                 </div>
@@ -21,14 +21,20 @@ const ProductStyled = styled.div`
     background: red;
     width: 240px;
     height: 330px;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    padding: 20px 20px 10px 20px;
 
     .image {
-        width: 100px;
+        width: 100%;
         height: auto;
+        margin-top: 30px;
+        border: 3px solid yellow;
+
         img {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            object-fit: contain;
         }
     }
 

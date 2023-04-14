57151af934f27d24401f0e1c4d67346d37aa13d1
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-14 19:54:15 +0200

    ✨ F06 : NAVBAR > create <ToogleButton/> and add react-toastify

--- src/components/pages/order/Navbar/NavbarRightSide.js
+++ src/components/pages/order/Navbar/NavbarRightSide.js
@@ -1,25 +1,75 @@
-import styled from "styled-components"
-import Profile from "./Profile"
+import styled from "styled-components";
+import "react-toastify/dist/ReactToastify.css";
+import Profile from "./Profile";
+import ToggleButton from "./ToggleButton";
+import { ToastContainer, toast } from "react-toastify";
+import { theme } from "../../../../theme";
+import { FaUserSecret } from "react-icons/fa";
+import { useState } from "react";
 
 export default function NavbarRightSide({ username }) {
-  return (
-    <NavbarRightSideStyled className="right-side">
-      {/* <div className="admin-button">Admin Button</div> */}
-      <Profile username={username} />
-    </NavbarRightSideStyled>
-  )
+    const [isAdminMode, setIsAdminMode] = useState(false);
+
+    const handleAdminToggle = () => {
+        if (!isAdminMode) {
+            notify();
+        }
+        setIsAdminMode(!isAdminMode);
+    };
+
+    const notify = () => {
+        toast.info("Mode admin activé", {
+            icon: <FaUserSecret size={30} />,
+            theme: "dark",
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+
+    return (
+        <NavbarRightSideStyled className="right-side">
+            <ToastContainer className="toaster" bodyClassName="body-toast" />
+            <div className="admin-button">
+                <ToggleButton isChecked={isAdminMode} onToggle={handleAdminToggle} />
+            </div>
+            <Profile username={username} />
+        </NavbarRightSideStyled>
+    );
 }
 
 const NavbarRightSideStyled = styled.div`
-  display: flex;
-  align-items: center;
-  padding-right: 50px;
+    display: flex;
+    align-items: center;
+    padding-right: 50px;
 
-  /* .admin-button {
-    background: lightblue;
-  } */
+    .admin-button {
+        margin-right: 20px;
+    }
 
-  .profile {
-    background: yellow;
-  }
-`
+    .profile {
+        background: yellow;
+    }
+
+    .toaster {
+        max-width: 300px;
+    }
+
+    .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+        background: ${theme.colors.background_dark};
+    }
+
+    .body-toast {
+        .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+            margin-right: 20px;
+            margin-left: 5px;
+        }
+        div {
+            line-height: 1.3em;
+        }
+    }
+`;

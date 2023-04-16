a54ab4a9b83eee4798d41426f22b5ab9d15f4590
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-16 16:45:55 +0200

    💄 F07 : MAIN > Admin > add label

--- src/components/reusable-ui/Tab.js
+++ src/components/reusable-ui/Tab.js
@@ -1,49 +1,56 @@
-import styled from "styled-components";
-import { theme } from "../../theme";
+import React from "react"
+import styled from "styled-components"
+import { theme } from "../../theme"
 
-export default function Tab({ Icon, onClick, className }) {
-    return (
-        <TabStyled onClick={onClick} className={className}>
-            <div className="icon">{Icon}</div>
-        </TabStyled>
-    );
+export default function Tab({ label, Icon, onClick, className }) {
+  return (
+    <TabStyled onClick={onClick} className={className}>
+      <div className="icon">{Icon}</div>
+      {label && <span className="label">{label}</span>}
+    </TabStyled>
+  )
 }
 
 const TabStyled = styled.button`
-    background: ${theme.colors.white};
-    height: 43px;
-    padding: 0 22px;
+  height: 43px;
+  padding: 0 22px;
 
-    cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  cursor: pointer;
+
+  position: relative;
+  top: 1px;
+
+  // fonts
+  font-size: ${theme.fonts.size.P0};
+  color: ${theme.colors.greySemiDark};
+
+  background: ${theme.colors.white};
+  box-shadow: ${theme.shadows.subtle};
+
+  // border
+  border-width: 1px 1px 2px 1px;
+  border-style: solid;
+  border-color: ${theme.colors.greyLight};
+
+  // border-radius
+  border-radius: ${theme.borderRadius.round};
+  border-bottom-right-radius: 0px;
+  border-bottom-left-radius: 0px;
+
+  :hover {
+    border-bottom: 2px solid ${theme.colors.white};
+    text-decoration: underline;
+  }
+
+  .icon {
     display: flex;
-    justify-content: center;
-    align-items: center;
+  }
 
-    position: relative;
-    left: 5%;
-    top: 1px;
-
-    // fonts
-    font-size: ${theme.fonts.size.P0};
-    color: ${theme.colors.greySemiDark};
-
-    //border
-    border-width: 1px 1px 2px 1px;
-    border-style: solid;
-    border-color: ${theme.colors.greyLight};
-    box-shadow: ${theme.shadows.subtle};
-
-    //border-radius
-    border-radius: ${theme.borderRadius.round};
-    border-bottom-left-radius: 0px;
-    border-bottom-right-radius: 0px;
-
-    :hover {
-        border-bottom: 2px solid ${theme.colors.white};
-    }
-
-    .icon{
-        display: flex;
-    }
-`;
+  .label {
+    margin-left: 13px;
+  }
+`

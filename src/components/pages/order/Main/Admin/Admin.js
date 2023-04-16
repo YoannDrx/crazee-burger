9098c580db21497ee7e9bee666a34ff3e71b0196
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-16 16:30:40 +0200

    💄 F07 : MAIN > Admin > add click and active style

--- src/components/pages/order/Main/Admin/Admin.js
+++ src/components/pages/order/Main/Admin/Admin.js
@@ -1,20 +1,23 @@
-import styled from 'styled-components';
-import { theme } from '../../../../../theme';
-import AdminTabs from './AdminTabs';
-import AdminPanel from './AdminPanel';
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+import AdminTabs from "./AdminTabs";
+import AdminPanel from "./AdminPanel";
+import { useState } from "react";
 
 export default function Admin() {
-  return (
-    <AdminStyled>
-      <AdminTabs/>
-      <AdminPanel/>
-    </AdminStyled>
-    )
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    return (
+        <AdminStyled>
+            <AdminTabs isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+            {!isCollapsed && <AdminPanel />}
+        </AdminStyled>
+    );
 }
 
 const AdminStyled = styled.div`
-            position: absolute;
-            bottom: 0;
-            right: 0;
-            left: 0;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
 `;

013ae5fc2596e2f480e375bf45fb9df259c4f810
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-16 22:14:29 +0200

    ✨ F08 : Card > add delete button

--- src/components/reusable-ui/Card.js
+++ src/components/reusable-ui/Card.js
@@ -1,98 +1,119 @@
-import styled from "styled-components"
-import { theme } from "../../theme"
-import PrimaryButton from "./PrimaryButton"
+import styled from "styled-components";
+import { theme } from "../../theme";
+import PrimaryButton from "./PrimaryButton";
+import { TiDelete } from "react-icons/ti";
+import { useContext } from "react";
+import OrderContext from "../../context/OrderContext";
 
-export default function Card({ title, imageSource, leftDescription }) {
-  return (
-    <CardStyled className="produit">
-      <div className="image">
-        <img src={imageSource} alt={title} />
-      </div>
-      <div className="text-info">
-        <div className="title">{title}</div>
-        <div className="description">
-          <div className="left-description">{leftDescription}</div>
-          <div className="right-description">
-            <PrimaryButton className="primary-button" label={"Ajouter"} />
-          </div>
-        </div>
-      </div>
-    </CardStyled>
-  )
+export default function Card({ title, imageSource, leftDescription, onClick }) {
+    const { isModeAdmin } = useContext(OrderContext);
+
+    return (
+        <CardStyled className="produit">
+            {isModeAdmin && <TiDelete className="delete-icon" onClick={onClick} />}
+            <div className="image">
+                <img src={imageSource} alt={title} />
+            </div>
+            <div className="text-info">
+                <div className="title">{title}</div>
+                <div className="description">
+                    <div className="left-description">{leftDescription}</div>
+                    <div className="right-description">
+                        <PrimaryButton className="primary-button" label={"Ajouter"} />
+                    </div>
+                </div>
+            </div>
+        </CardStyled>
+    );
 }
 
 const CardStyled = styled.div`
-  background: ${theme.colors.white};
-  width: 240px;
-  height: 330px;
-  display: grid;
-  grid-template-rows: 65% 1fr;
-  padding: 20px;
-  padding-bottom: 10px;
-  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-  border-radius: ${theme.borderRadius.extraRound};
-
-  .image {
-    width: 100%;
-    height: auto;
-    margin-top: 30px;
-    margin-bottom: 20px;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-    }
-  }
-
-  .text-info {
+    background: ${theme.colors.white};
+    width: 240px;
+    height: 330px;
     display: grid;
-    grid-template-rows: 30% 70%;
-    padding: 5px;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
+    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+    border-radius: ${theme.borderRadius.extraRound};
+    border: 3px solid red;
+    position: relative;
 
-    .title {
-      margin: auto 0;
-      font-size: ${theme.fonts.size.P4};
-      position: relative;
-      bottom: 10px;
-      font-weight: ${theme.fonts.weights.bold};
-      color: ${theme.colors.dark};
-      text-align: left;
-      white-space: nowrap;
-      overflow: hidden;
-      width: 100%;
-      text-overflow: ellipsis;
-      font-family: "Amatic SC", cursive;
-    }
-
-    .description {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-
-      .left-description {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        font-weight: ${theme.fonts.weights.medium};
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        font-weight: ${theme.fonts.weights.medium};
+    .delete-icon {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        font-size: ${theme.fonts.size.P3};
         color: ${theme.colors.primary};
-      }
+        cursor: pointer;
 
-      .right-description {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        font-size: ${theme.fonts.size.P1};
-
-        .primary-button {
-          font-size: ${theme.fonts.size.XS};
-          cursor: pointer;
-          padding: 12px;
+        &:hover {
+            color: red;
         }
-      }
     }
-  }
-`
+
+    .image {
+        width: 100%;
+        height: auto;
+        margin-top: 30px;
+        margin-bottom: 20px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+    }
+
+    .text-info {
+        display: grid;
+        grid-template-rows: 30% 70%;
+        padding: 5px;
+
+        .title {
+            margin: auto 0;
+            font-size: ${theme.fonts.size.P4};
+            position: relative;
+            bottom: 10px;
+            font-weight: ${theme.fonts.weights.bold};
+            color: ${theme.colors.dark};
+            text-align: left;
+            white-space: nowrap;
+            overflow: hidden;
+            width: 100%;
+            text-overflow: ellipsis;
+            font-family: "Amatic SC", cursive;
+        }
+
+        .description {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+
+            .left-description {
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+                font-weight: ${theme.fonts.weights.medium};
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                font-weight: ${theme.fonts.weights.medium};
+                color: ${theme.colors.primary};
+            }
+
+            .right-description {
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+                font-size: ${theme.fonts.size.P1};
+
+                .primary-button {
+                    font-size: ${theme.fonts.size.XS};
+                    cursor: pointer;
+                    padding: 12px;
+                }
+            }
+        }
+    }
+`;

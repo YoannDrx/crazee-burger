091dc1428cb6da5cbf7e11210bb5246d5d6c39aa
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-04-17 14:47:26 +0200

    ✨ F08 : ADMIN > create add product form

--- src/components/reusable-ui/TextInput.js
+++ src/components/reusable-ui/TextInput.js
@@ -1,38 +1,38 @@
-import styled from "styled-components";
-import { theme } from "../../theme";
+import styled from "styled-components"
+import { theme } from "../../theme"
 
-export default function TextInput({ value, onChange, Icon, ...extraProps }) {
-    return (
-        <InputStyled>
-            {Icon && Icon}
-            <input onChange={onChange} type="text" {...extraProps} />
-        </InputStyled>
-    );
+export default function TextInput({ value, onChange, Icon, style, ...extraProps }) {
+  return (
+    <InputStyled style={style}>
+      {Icon && Icon}
+      <input onChange={onChange} type="text" {...extraProps} />
+    </InputStyled>
+  )
 }
 
 const InputStyled = styled.div`
-    background-color: #fff;
-    border-radius: ${theme.borderRadius.round};
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
+  background-color: #fff;
+  border-radius: ${theme.borderRadius.round};
+  display: flex;
+  align-items: center;
+  padding: 18px 24px;
+  margin: 18px 0;
 
-    .icon {
-        font-size: ${theme.fonts.size.SM};
-        margin-right: 8px;
-        color: ${theme.colors.greySemiDark};
+  .icon {
+    font-size: ${theme.fonts.size.SM};
+    margin-right: 8px;
+    color: ${theme.colors.greySemiDark};
+  }
+
+  input {
+    border: none;
+    font-size: ${theme.fonts.size.SM};
+    color: ${theme.colors.dark};
+    width: 100%;
+
+    &::placeholder {
+      background: ${theme.colors.white};
+      color: ${theme.colors.greyMedium};
     }
-
-    input {
-        border: none;
-        font-size: ${theme.fonts.size.SM};
-        color: ${theme.colors.dark};
-        width: 100%;
-
-        &::placeholder {
-            background: ${theme.colors.white};
-            color: ${theme.colors.greyMedium};
-        }
-    }
-`;
+  }
+`

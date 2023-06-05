4e2709db8554254b4ceebb4de2c37e18db21aa27
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-05 15:24:07 +0200

    🚧  F08 : ADMIN > AddForm > add newproduct

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
+import OrderContext from "../../../../../../../context/OrderContext";
 
 export default function AddForm() {
+  const {handleAdd} = useContext(OrderContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAdd(newProduct)
+};
+
+const newProduct = {
+  id: new Date().getTime(),
+  title: "Nouveau produit",
+  imageSource: "https://img1.freepng.fr/20180721/zbv/kisspng-cheeseburger-hamburger-whopper-fast-food-ham-and-c-big-burger-5b5390e2a00617.5043896615322032346555.jpg",
+  price: 2.5,
+};
+
     return (
-        <AddFormStyled>
+        <AddFormStyled onSubmit={handleSubmit}>
             <div className="image-preview">image-preview</div>
             <div className="input-fields">
                 <input type="text" placeholder="nom"></input>
--- src/components/pages/order/Main/MainRightSide/Menu.js
+++ src/components/pages/order/Main/MainRightSide/Menu.js
@@ -1,37 +1,30 @@
-import { useState } from "react"
-import styled from "styled-components"
-import { fakeMenu } from "../../../../../fakeData/fakeMenu"
-import { theme } from "../../../../../theme"
-import { formatPrice } from "../../../../../utils/maths"
-import Card from "../../../../reusable-ui/Card"
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+import { formatPrice } from "../../../../../utils/maths";
+import Card from "../../../../reusable-ui/Card";
+import { useContext } from "react";
+import OrderContext from "../../../../../context/OrderContext";
 
 export default function Menu() {
-  const [menu, setMenu] = useState(fakeMenu.LARGE)
+const {menu} = useContext(OrderContext)
 
-  return (
-    <MenuStyled className="menu">
-      {menu.map(({ id, title, imageSource, price }) => {
-        return (
-          <Card
-            key={id}
-            title={title}
-            imageSource={imageSource}
-            leftDescription={formatPrice(price)}
-          />
-        )
-      })}
-    </MenuStyled>
-  )
+    return (
+        <MenuStyled className="menu">
+            {menu.map(({ id, title, imageSource, price }) => {
+                return <Card key={id} title={title} imageSource={imageSource} leftDescription={formatPrice(price)} />;
+            })}
+        </MenuStyled>
+    );
 }
 
 const MenuStyled = styled.div`
-  background: ${theme.colors.background_white};
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-row-gap: 60px;
-  padding: 50px 50px 150px;
-  justify-items: center;
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  overflow-y: scroll;
-`
+    background: ${theme.colors.background_white};
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-row-gap: 60px;
+    padding: 50px 50px 150px;
+    justify-items: center;
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    overflow-y: scroll;
+`;

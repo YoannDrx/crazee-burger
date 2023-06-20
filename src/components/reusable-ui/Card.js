76c1f2c09ff22ff5014325d4e9c772ff6866d3f4
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-20 17:15:46 +0200

    🚧 F09 : ADMIN > EditFom > retrive info of card clicked on

--- src/components/reusable-ui/Card.js
+++ src/components/reusable-ui/Card.js
@@ -3,9 +3,9 @@ import { theme } from "../../theme"
 import Button from "./Button"
 import { TiDelete } from "react-icons/ti"
 
-export default function Card({ title, imageSource, leftDescription, hasDeleteButton, onDelete }) {
+export default function Card({ title, imageSource, leftDescription, hasDeleteButton, onDelete, onClick }) {
   return (
-    <CardStyled className="produit">
+    <CardStyled className="produit" onClick={onClick}>
       {hasDeleteButton && (
         <button className="delete-button" aria-label="delete-button" onClick={onDelete}>
           <TiDelete className="icon" />

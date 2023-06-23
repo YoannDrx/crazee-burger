407334f57acc540d097baf1fe2b39fb3c35a08a1
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-23 13:15:54 +0200

    ♻️ F09 : BUTTON > move height 100%  inside succes vairant only

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
@@ -5,62 +5,57 @@ import ImagePreview from "./ImagePreview";
 import { getInputTextsConfig } from "./inputTextConfig";
 
 const Form = React.forwardRef(({ product, onSubmit, onChange, children }, ref) => {
-	// state
+    // state
 
-	// comportements
-	const inputTexts = getInputTextsConfig(product);
+    // comportements
+    const inputTexts = getInputTextsConfig(product);
 
-	// affichage
-	return (
-		<FormStyled onSubmit={onSubmit}>
-			<ImagePreview imageSource={product.imageSource} title={product.title} />
-			<div className="input-fields">
-				{inputTexts.map((input) => (
-					<TextInput
-						{...input}
-						key={input.id}
-						onChange={onChange}
-						version="minimalist"
-						ref={ref && input.name === "title" ? ref : null}
-					/>
-				))}
-			</div>
-			<div className="submit">{children}</div>
-		</FormStyled>
-	);
+    // affichage
+    return (
+        <FormStyled onSubmit={onSubmit}>
+            <ImagePreview imageSource={product.imageSource} title={product.title} />
+            <div className="input-fields">
+                {inputTexts.map((input) => (
+                    <TextInput
+                        {...input}
+                        key={input.id}
+                        onChange={onChange}
+                        version="minimalist"
+                        ref={ref && input.name === "title" ? ref : null}
+                    />
+                ))}
+            </div>
+            <div className="form-footer">{children}</div>
+        </FormStyled>
+    );
 });
 
 export default Form;
 
 const FormStyled = styled.form`
-	/* border: 2px solid black; */
-	display: grid;
-	grid-template-columns: 1fr 3fr;
-	grid-template-rows: repeat(4, 1fr);
-	height: 100%;
-	width: 70%;
-	grid-column-gap: 20px;
-	grid-row-gap: 8px;
+    /* border: 2px solid black; */
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    height: 100%;
+    width: 70%;
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
 
-	.input-fields {
-		/* background: blue; */
-		grid-area: 1 / 2 / -2 / 3;
+    .input-fields {
+        /* background: blue; */
+        grid-area: 1 / 2 / -2 / 3;
 
-		display: grid;
-		grid-row-gap: 8px;
-	}
+        display: grid;
+        grid-row-gap: 8px;
+    }
 
-	.submit {
-		/* background: green; */
-		grid-area: 4 / -2 / -1 / -1;
-		display: flex;
-		align-items: center;
-		position: relative;
-		top: 3px;
-
-		.submit-button {
-			/* width: 50%; */
-			height: 100%;
-		}
-	}
+    .form-footer {
+        /* background: green; */
+        grid-area: 4 / -2 / -1 / -1;
+        display: flex;
+        align-items: center;
+        position: relative;
+        top: 3px;
+    }
 `;
--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/HintMessage.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/HintMessage.js
@@ -3,24 +3,24 @@ import { theme } from "../../../../../../../theme";
 import { HiCursorClick } from "react-icons/hi";
 
 export default function HintMessage() {
-    return (
-        <HintMessageStyled>
-            <span>Cliquer sur un porduit pour le modifier</span>
-            <HiCursorClick />
-        </HintMessageStyled>
-    );
+	return (
+		<HintMessageStyled>
+			<span>Cliquer sur un porduit pour le modifier</span>
+			<HiCursorClick />
+		</HintMessageStyled>
+	);
 }
 
 const HintMessageStyled = styled.div`
-    display: flex;
-    align-items: center;
-    font-family: ${theme.fonts.family.stylish};
-    font-size: ${theme.fonts.size.P3};
-    color: ${theme.colors.greyBlue};
-    position: relative;
-    top: 50px;
+	display: flex;
+	align-items: center;
+	font-family: ${theme.fonts.family.stylish};
+	font-size: ${theme.fonts.size.P3};
+	color: ${theme.colors.greyBlue};
+	position: relative;
+	top: 50px;
 
-    span {
-        margin-right: 10px;
-    }
+	span {
+		margin-right: 10px;
+	}
 `;

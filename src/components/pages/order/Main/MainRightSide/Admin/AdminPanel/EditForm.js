7877338808597f97bf7b0ffdeb96a7aa851438f1
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-23 12:32:49 +0200

    ♻️ F09 : ADMIN > Form > make reusable form for edit and add form

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
@@ -5,68 +5,55 @@ import styled from "styled-components";
 import TextInput from "../../../../../../reusable-ui/TextInput";
 import ImagePreview from "./ImagePreview";
 import EditInfoMessage from "./EditInfoMessage";
+import Form from "./Form";
 
 export default function EditForm() {
-    const { productSelected, setProductSelected, handleEdit, titleEditRef } =
-        useContext(OrderContext);
-    const inputTexts = getInputTextsConfig(productSelected);
+	const { productSelected, setProductSelected, handleEdit, titleEditRef } =
+		useContext(OrderContext);
+	const inputTexts = getInputTextsConfig(productSelected);
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        const productBeingUpdated = { ...productSelected, [name]: value };
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		const productBeingUpdated = { ...productSelected, [name]: value };
 
-        setProductSelected(productBeingUpdated);
-        handleEdit(productBeingUpdated);
-    };
+		setProductSelected(productBeingUpdated);
+		handleEdit(productBeingUpdated);
+	};
 
-    return (
-        <EditFormStyled>
-            <ImagePreview
-                imageSource={productSelected.imageSource}
-                title={productSelected.title}
-            />
-            <div className="input-fields">
-                {inputTexts.map((input) => (
-                    <TextInput
-                        {...input}
-                        key={input.id}
-                        onChange={handleChange}
-                        version="minimalist"
-                        ref={input.name === "title" ? titleEditRef : null}
-                    />
-                ))}
-            </div>
-            <div className="submit">
-                <EditInfoMessage />
-            </div>
-        </EditFormStyled>
-    );
+	return (
+		<Form
+			product={productSelected}
+			onChange={handleChange}
+			ref={titleEditRef}
+            QUELQUECHOSE={<EditInfoMessage/>}
+		/>
+	);
 }
 
 const EditFormStyled = styled.form`
-    /* border: 2px solid black; */
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    grid-template-rows: repeat(4, 1fr);
-    height: 100%;
-    width: 70%;
-    grid-column-gap: 20px;
-    grid-row-gap: 8px;
+	/* border: 2px solid black; */
+	display: grid;
+	grid-template-columns: 1fr 3fr;
+	grid-template-rows: repeat(4, 1fr);
+	height: 100%;
+	width: 70%;
+	grid-column-gap: 20px;
+	grid-row-gap: 8px;
 
-    .input-fields {
-        /* background: blue; */
-        grid-area: 1 / 2 / -2 / 3;
+	.input-fields {
+		/* background: blue; */
+		grid-area: 1 / 2 / -2 / 3;
 
-        display: grid;
-        grid-row-gap: 8px;
-    }
+		display: grid;
+		grid-row-gap: 8px;
+	}
 
-    .submit {
-        /* background: green; */
-        grid-area: 4 / -2 / -1 / -1;
-        display: flex;
-        align-items: center;
-        position: relative;
-        top: 3px;
-    }
+	.submit {
+		/* background: green; */
+		grid-area: 4 / -2 / -1 / -1;
+		display: flex;
+		align-items: center;
+		position: relative;
+		top: 3px;
+	}
 `;
--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
@@ -1,76 +1,72 @@
-import { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import TextInput from "../../../../../../reusable-ui/TextInput";
-import Button from "../../../../../../reusable-ui/Button";
 import ImagePreview from "./ImagePreview";
-import SubmitMessage from "./SubmitMessage";
 import { getInputTextsConfig } from "./inputTextConfig";
 
-export default function Form({ product, onSubmit, onChange, isSubmitted }) {
-    // state
+const Form = React.forwardRef(
+	({ product, onSubmit, onChange, isSubmitted, QUELQUECHOSE }, ref) => {
+		// state
 
-    // comportements
+		// comportements
 
-    const inputTexts = getInputTextsConfig(product);
+		const inputTexts = getInputTextsConfig(product);
 
-    // affichage
-    return (
-        <FormStyled onSubmit={onSubmit}>
-            <ImagePreview
-                imageSource={product.imageSource}
-                title={product.title}
-            />
-            <div className="input-fields">
-                {inputTexts.map((input) => (
-                    <TextInput
-                        {...input}
-                        key={input.id}
-                        onChange={onChange}
-                        version="minimalist"
-                    />
-                ))}
-            </div>
-            <div className="submit">
-                <Button
-                    className="submit-button"
-                    label={"Ajouter un nouveau produit au menu"}
-                    version="success"
-                />
-                {isSubmitted && <SubmitMessage />}
-            </div>
-        </FormStyled>
-    );
-}
+		// affichage
+		return (
+			<FormStyled onSubmit={onSubmit}>
+				<ImagePreview
+					imageSource={product.imageSource}
+					title={product.title}
+				/>
+				<div className="input-fields">
+					{inputTexts.map((input) => (
+						<TextInput
+							{...input}
+							key={input.id}
+							onChange={onChange}
+							version="minimalist"
+							ref={ref && input.name === "title" ? ref : null}
+						/>
+					))}
+				</div>
+				<div className="submit">{QUELQUECHOSE}</div>
+			</FormStyled>
+		);
+	}
+);
+
+export default Form;
 
 const FormStyled = styled.form`
-    /* border: 2px solid black; */
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    grid-template-rows: repeat(4, 1fr);
-    height: 100%;
-    width: 70%;
-    grid-column-gap: 20px;
-    grid-row-gap: 8px;
+	/* border: 2px solid black; */
+	display: grid;
+	grid-template-columns: 1fr 3fr;
+	grid-template-rows: repeat(4, 1fr);
+	height: 100%;
+	width: 70%;
+	grid-column-gap: 20px;
+	grid-row-gap: 8px;
 
-    .input-fields {
-        /* background: blue; */
-        grid-area: 1 / 2 / -2 / 3;
+	.input-fields {
+		/* background: blue; */
+		grid-area: 1 / 2 / -2 / 3;
 
-        display: grid;
-        grid-row-gap: 8px;
-    }
+		display: grid;
+		grid-row-gap: 8px;
+	}
 
-    .submit {
-        /* background: green; */
-        grid-area: 4 / -2 / -1 / -1;
-        display: flex;
-        align-items: center;
-        position: relative;
-        top: 3px;
+	.submit {
+		/* background: green; */
+		grid-area: 4 / -2 / -1 / -1;
+		display: flex;
+		align-items: center;
+		position: relative;
+		top: 3px;
 
-        .submit-button {
-            /* width: 50%; */
-            height: 100%;
-        }
-    }
+		.submit-button {
+			/* width: 50%; */
+			height: 100%;
+		}
+	}
 `;

fa8fd999a7914f50a5c7ba6707f75765f97ca22d
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-23 17:03:25 +0200

    ♻️ F10 : BASKET > refacto Header

---
+++ src/components/pages/order/Main/Basket/Basket.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+import Header from "../../../../reusable-ui/Header";
+
+export default function Basket() {
+	return (
+		<BasketStyled>
+			<Header />
+			<div className="body">Body</div>
+			<Header />
+		</BasketStyled>
+	);
+}
+
+const BasketStyled = styled.div`
+	background: pink;
+	display: flex;
+	flex-direction: column;
+
+	.body {
+		flex: 1;
+		background: ${theme.colors.background_white};
+		box-shadow: ${theme.shadows.basket};
+	}
+`;

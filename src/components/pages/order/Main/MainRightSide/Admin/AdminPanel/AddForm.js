6a329d10677b1509cc7cb312addb475889bb2f9e
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-23 12:32:49 +0200

    ♻️ F09 : ADMIN > Form > make reusable form for edit and add form

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.js
@@ -2,45 +2,57 @@ import { useContext, useState } from "react";
 import OrderContext from "../../../../../../../context/OrderContext";
 import { EMPTY_PRODUCT } from "../../../../../../../enums/product";
 import Form from "./Form";
+import Button from "../../../../../../reusable-ui/Button";
+import SubmitMessage from "./SubmitMessage";
 
 export default function AddForm() {
-    // state
-    const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext);
-    const [isSubmitted, setIsSubmitted] = useState(false);
+	// state
+	const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext);
+	const [isSubmitted, setIsSubmitted] = useState(false);
 
-    // comportements
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const newProductToAdd = {
-            ...newProduct,
-            id: crypto.randomUUID(),
-        };
+	// comportements
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const newProductToAdd = {
+			...newProduct,
+			id: crypto.randomUUID(),
+		};
 
-        handleAdd(newProductToAdd);
-        setNewProduct(EMPTY_PRODUCT);
+		handleAdd(newProductToAdd);
+		setNewProduct(EMPTY_PRODUCT);
 
-        displaySuccessMessage();
-    };
+		displaySuccessMessage();
+	};
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setNewProduct({ ...newProduct, [name]: value });
-    };
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setNewProduct({ ...newProduct, [name]: value });
+	};
 
-    const displaySuccessMessage = () => {
-        setIsSubmitted(true);
-        setTimeout(() => {
-            setIsSubmitted(false);
-        }, 2000);
-    };
+	const displaySuccessMessage = () => {
+		setIsSubmitted(true);
+		setTimeout(() => {
+			setIsSubmitted(false);
+		}, 2000);
+	};
 
-    // affichage
-    return (
-        <Form
-            onSunbmit={handleSubmit}
-            onChange={handleChange}
-            product={newProduct}
-            isSubmitted={isSubmitted}
-        />
-    );
+	// affichage
+	return (
+		<Form
+			onSunbmit={handleSubmit}
+			onChange={handleChange}
+			product={newProduct}
+			isSubmitted={isSubmitted}
+			QUELQUECHOSE={
+				<>
+					<Button
+						className="submit-button"
+						label={"Ajouter un nouveau produit au menu"}
+						version="success"
+					/>
+					{isSubmitted && <SubmitMessage />}
+				</>
+			}
+		/>
+	);
 }

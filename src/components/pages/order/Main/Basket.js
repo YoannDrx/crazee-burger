422e42f0f0a102e985ac0e6aa1f219c642369e91
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-23 16:38:23 +0200

    💄 F10 : BASKET > add layout

--- src/components/pages/order/Main/Basket.js
+++ src/components/pages/order/Main/Basket.js
@@ -1,9 +1,27 @@
 import styled from "styled-components"
 
 export default function Basket() {
-  return <BasketStyled>Basket</BasketStyled>
+  return <BasketStyled>
+    <div className="head">Head</div>
+    <div className="body">Body</div>
+    <div className="footer">Footer</div>
+  </BasketStyled>
 }
 
 const BasketStyled = styled.div`
   background: pink;
+  display: flex;
+  flex-direction: column;
+
+  .head{
+    border: 3px solid red;
+  }
+
+  .body{
+    border: 3px solid blue;
+  }
+
+  .footer{
+    border: 3px solid green;
+  }
 `

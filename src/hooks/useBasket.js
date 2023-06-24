99ca51bf41d9a6b037d8508e9dbb4fb9a9358cc5
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-24 16:26:44 +0200

    🚧 F11 : Render Basket conditionnaly if empty or not

--- src/hooks/useBasket.js
+++ src/hooks/useBasket.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { fakeBasket } from "../fakeData/fakeBasket";
 
 export const useBasket = () => {
-	const [basket, setBasket] = useState(fakeBasket.LARGE);
+	const [basket, setBasket] = useState(fakeBasket.EMPTY);
 
 	return { basket };
 };

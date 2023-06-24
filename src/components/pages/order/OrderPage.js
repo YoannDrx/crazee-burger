1d384e7611ecdede033a2b7ee3ac02d479451c0e
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-24 16:22:35 +0200

    ♻️ F11 : BASKET > rename basketBody to EmptyBasket

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -6,65 +6,67 @@ import Navbar from "./Navbar/Navbar";
 import OrderContext from "../../../context/OrderContext";
 import { EMPTY_PRODUCT } from "../../../enums/product";
 import { useMenu } from "../../../hooks/useMenu";
+import { useBasket } from "../../../hooks/useBasket";
 
 export default function OrderPage() {
-    // state
-    const [isModeAdmin, setIsModeAdmin] = useState(false);
-    const [isCollapsed, setIsCollapsed] = useState(false);
-    const [currentTabSelected, setCurrentTabSelected] = useState("add");
-    const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
-    const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
-    const titleEditRef = useRef();
-    const {menu, handleAdd, handleDelete, handleEdit, resetMenu} = useMenu()
-    
+	// state
+	const [isModeAdmin, setIsModeAdmin] = useState(false);
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [currentTabSelected, setCurrentTabSelected] = useState("add");
+	const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+	const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
+	const titleEditRef = useRef();
+	const { menu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu();
+	const { basket } = useBasket();
 
-    // comportements
+	// comportements
 
-    const orderContextValue = {
-        isModeAdmin,
-        setIsModeAdmin,
-        isCollapsed,
-        setIsCollapsed,
-        currentTabSelected,
-        setCurrentTabSelected,
-        menu,
-        handleAdd,
-        handleDelete,
-        resetMenu,
-        newProduct,
-        setNewProduct,
-        productSelected,
-        setProductSelected,
-        handleEdit,
-        titleEditRef,
-    };
+	const orderContextValue = {
+		isModeAdmin,
+		setIsModeAdmin,
+		isCollapsed,
+		setIsCollapsed,
+		currentTabSelected,
+		setCurrentTabSelected,
+		menu,
+		handleAdd,
+		handleDelete,
+		resetMenu,
+		newProduct,
+		setNewProduct,
+		productSelected,
+		setProductSelected,
+		handleEdit,
+		titleEditRef,
+		basket,
+	};
 
-    //affichage
-    return (
-        <OrderContext.Provider value={orderContextValue}>
-            <OrderPageStyled>
-                <div className="container">
-                    <Navbar />
-                    <Main />
-                </div>
-            </OrderPageStyled>
-        </OrderContext.Provider>
-    );
+	//affichage
+	return (
+		<OrderContext.Provider value={orderContextValue}>
+			<OrderPageStyled>
+				<div className="container">
+					<Navbar />
+					<Main />
+				</div>
+			</OrderPageStyled>
+		</OrderContext.Provider>
+	);
 }
 
 const OrderPageStyled = styled.div`
-    background: ${theme.colors.primary};
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	background: ${theme.colors.primary};
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-    .container {
-        background: red;
-        height: 95vh;
-        width: 1400px;
-        display: flex;
-        flex-direction: column;
-        border-radius: ${theme.borderRadius.extraRound};
-    }
+	.container {
+		background: red;
+		height: 95vh;
+		width: 1400px;
+		display: flex;
+		flex-direction: column;
+		border-radius: ${theme.borderRadius.extraRound};
+	}
 `;

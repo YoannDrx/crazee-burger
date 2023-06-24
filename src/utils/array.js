d3a3d8b5675854db5b438ed72b87e07cda004864
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-24 18:05:14 +0200

    ✨ F11 : BASKET > CRUD product

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,7 +1,26 @@
-export const deeplClone = (array) => {
-	return JSON.parse(JSON.stringify(array));
-};
+export const deepClone = (array) => {
+  return JSON.parse(JSON.stringify(array))
+}
 
-export const findInArray = (id, array) => {
-	return array.find((itemInArray) => itemInArray.id === id);
-};
+export const find = (id, array) => {
+  return array.find((itemInArray) => itemInArray.id === id)
+}
+
+export const findIndex = (idWithUnknowwIndex, array) => {
+  return array.findIndex((itemInArray) => itemInArray.id === idWithUnknowwIndex)
+}
+
+export const filter = (idOfItemToRemove, array) => {
+  return array.filter((item) => item.id !== idOfItemToRemove)
+}
+
+// const fruits = [{ nom: "Abricot" }, { nom: "Banane" }]
+// const fruitsShallowCopy = [...fruits]
+// const fruitsDeepCopy = deepClone(fruits)
+
+// //fruitsShallowCopy[1].nom = "Cerise"
+// fruitsDeepCopy[1].nom = "Cerise"
+
+// console.log("fruits: ", fruits)
+// //console.log("fruitsShallowCopy: ", fruitsShallowCopy)
+// console.log("fruitsDeepCopy: ", fruitsDeepCopy)

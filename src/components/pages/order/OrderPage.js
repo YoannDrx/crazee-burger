413a4d67c9846b615899c128566d2a4aad970c01
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-24 17:43:29 +0200

    ✨ F11 : BASKET > create handleAddToBasket when product is already in basket

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -17,7 +17,7 @@ export default function OrderPage() {
 	const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
 	const titleEditRef = useRef();
 	const { menu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu();
-	const { basket } = useBasket();
+	const { basket, handleAddToBasket } = useBasket();
 
 	// comportements
 
@@ -39,6 +39,7 @@ export default function OrderPage() {
 		handleEdit,
 		titleEditRef,
 		basket,
+		handleAddToBasket
 	};
 
 	//affichage

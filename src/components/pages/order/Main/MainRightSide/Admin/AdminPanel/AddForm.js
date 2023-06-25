8f1982b4e846021e6a28a93f91eef65216c48c59
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-25 18:27:06 +0200

    ✨ F11 : ticket done

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.js
@@ -1,42 +1,38 @@
-import React, { useContext } from "react";
-import OrderContext from "../../../../../../../context/OrderContext";
-import { EMPTY_PRODUCT } from "../../../../../../../enums/product";
-import Button from "../../../../../../reusable-ui/Button";
-import Form from "./Form";
-import SubmitMessage from "./SubmitMessage";
-import { useSuccessMessage } from "../../../../../../../hooks/useSuccesMessage";
+import React, { useContext } from "react"
+import OrderContext from "../../../../../../../context/OrderContext"
+import { EMPTY_PRODUCT } from "../../../../../../../enums/product"
+import { useSuccessMessage } from "../../../../../../../hooks/useSuccessMessage"
+import Form from "./Form"
+import SubmitButton from "./SubmitButton"
 
 export default function AddForm() {
-	// state
-	const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext);
-	const { isSubmitted, displaySuccessMessage } = useSuccessMessage(5000);
+  // state
+  const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext)
+  const { isSubmitted, displaySuccessMessage } = useSuccessMessage()
 
-	// comportements
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const newProductToAdd = {
-			...newProduct,
-			id: crypto.randomUUID(),
-		};
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newProductToAdd = {
+      ...newProduct,
+      id: crypto.randomUUID(),
+    }
 
-		handleAdd(newProductToAdd);
-		setNewProduct(EMPTY_PRODUCT);
+    handleAdd(newProductToAdd)
+    setNewProduct(EMPTY_PRODUCT)
 
-		displaySuccessMessage();
-	};
+    displaySuccessMessage()
+  }
 
-	const handleChange = (event) => {
-		const { name, value } = event.target;
-		setNewProduct({ ...newProduct, [name]: value });
-	};
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setNewProduct({ ...newProduct, [name]: value })
+  }
 
-	// affichage
-	return (
-		<Form product={newProduct} onSubmit={handleSubmit} onChange={handleChange}>
-			<>
-				<Button className="submit-button" label={"Ajouter un nouveau produit au menu"} version="success" />
-				{isSubmitted && <SubmitMessage />}
-			</>
-		</Form>
-	);
+  // affichage
+  return (
+    <Form product={newProduct} onSubmit={handleSubmit} onChange={handleChange}>
+      <SubmitButton isSubmitted={isSubmitted} />
+    </Form>
+  )
 }

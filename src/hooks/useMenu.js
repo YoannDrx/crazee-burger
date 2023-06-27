77f2b795a2f21d2be78302a9a856bdb8fb52d0bd
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-27 16:35:24 +0200

    🚧 F13 : MENU > firestore > add Update CRUD

--- src/hooks/useMenu.js
+++ src/hooks/useMenu.js
@@ -32,7 +32,7 @@ export const useMenu = () => {
     syncBothMenus(username, menuUpdated)
   }
 
-  const handleEdit = (productBeingEdited) => {
+  const handleEdit = (productBeingEdited, username) => {
     // 1. copie du state (deep clone)
     const menuCopy = deepClone(menu)
 
@@ -44,7 +44,7 @@ export const useMenu = () => {
 
     // 3. update du state
     setMenu(menuCopy)
-    syncBothMenus(menuCopy)
+    syncBothMenus(username, menuCopy)
   }
 
   const resetMenu = (username) => {

bf8db35c4f1c0a693207083e784bc6bf91b90f9e
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-27 17:11:48 +0200

    🚧 F13 : MENU > add saving message

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
@@ -1,29 +1,51 @@
-import { useContext } from "react"
-import OrderContext from "../../../../../../../context/OrderContext"
-import Form from "./Form"
-import EditInfoMessage from "./EditInfoMessage"
+import { useContext, useState } from "react";
+import OrderContext from "../../../../../../../context/OrderContext";
+import Form from "./Form";
+import EditInfoMessage from "./EditInfoMessage";
+import SavingMessage from "./SavingMessage";
+import { useSuccessMessage } from "../../../../../../../hooks/useSuccessMessage";
 
 export default function EditForm() {
-  // state
-  const { username, productSelected, setProductSelected, handleEdit, titleEditRef } = useContext(OrderContext)
+	// state
+	const { username, productSelected, setProductSelected, handleEdit, titleEditRef } = useContext(OrderContext);
+	const [valueOnFocus, setValueOnFocus] = useState();
+	const { isSubmitted: isSaved, displaySuccessMessage } = useSuccessMessage();
 
-  // comportements (gestionnaires d'événement ou "event handlers")
-  const handleChange = (event) => {
-    const { name, value } = event.target
+	// comportements (gestionnaires d'événement ou "event handlers")
+	const handleChange = (event) => {
+		const { name, value } = event.target;
 
-    const productBeingUpdated = {
-      ...productSelected,
-      [name]: value,
-    }
+		const productBeingUpdated = {
+			...productSelected,
+			[name]: value,
+		};
 
-    setProductSelected(productBeingUpdated) // cette ligne update le formulaire
-    handleEdit(productBeingUpdated, username) // cette ligne update le menu
-  }
+		setProductSelected(productBeingUpdated); // cette ligne update le formulaire
+		handleEdit(productBeingUpdated, username); // cette ligne update le menu
+	};
 
-  // affichage
-  return (
-    <Form product={productSelected} onChange={handleChange} ref={titleEditRef}>
-      <EditInfoMessage />
-    </Form>
-  )
+	const handleOnFocus = (event) => {
+		const inputValueOnFocus = event.target.value;
+		setValueOnFocus(inputValueOnFocus);
+	};
+
+	const handleOnBlur = (event) => {
+		const valueOnBlur = event.target.value;
+		if (valueOnFocus !== valueOnBlur) {
+		}
+		displaySuccessMessage();
+	};
+
+	// affichage
+	return (
+		<Form
+			product={productSelected}
+			onChange={handleChange}
+			onFocus={handleOnFocus}
+			onBlur={handleOnBlur}
+			ref={titleEditRef}
+		>
+			{isSaved ? <SavingMessage /> : <EditInfoMessage />}
+		</Form>
+	);
 }

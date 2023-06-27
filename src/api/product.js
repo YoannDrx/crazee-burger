a92ad44e4687879e18d68e0ee18483199140ffd8
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-27 15:59:34 +0200

    🚧  F13 : MENU > firestore  > read in CRUD

--- src/api/product.js
+++ src/api/product.js
@@ -1,4 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebase-config";
 
 export const syncBothMenus = (userId, menuUpdated) => {
@@ -10,4 +10,14 @@ export const syncBothMenus = (userId, menuUpdated) => {
 	};
 
 	setDoc(docRef, newDoc);
+};
+
+export const getMenu = async (idUser) => {
+	const docRef = doc(db, "users", idUser);
+	const docSnapshot = await getDoc(docRef);
+
+	if (docSnapshot.exists()) {
+		const {menu} = docSnapshot.data();
+		return menu
+	}
 };
--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,86 +1,96 @@
-import { useRef, useState } from "react"
-import styled from "styled-components"
-import { theme } from "../../../theme"
-import Main from "./Main/Main"
-import Navbar from "./Navbar/Navbar"
-import OrderContext from "../../../context/OrderContext"
-import { EMPTY_PRODUCT } from "../../../enums/product"
-import { useMenu } from "../../../hooks/useMenu"
-import { useBasket } from "../../../hooks/useBasket"
-import { findObjectById } from "../../../utils/array"
-import { useParams } from "react-router-dom"
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { theme } from "../../../theme";
+import Main from "./Main/Main";
+import Navbar from "./Navbar/Navbar";
+import OrderContext from "../../../context/OrderContext";
+import { EMPTY_PRODUCT } from "../../../enums/product";
+import { useMenu } from "../../../hooks/useMenu";
+import { useBasket } from "../../../hooks/useBasket";
+import { findObjectById } from "../../../utils/array";
+import { useParams } from "react-router-dom";
+import { getMenu } from "../../../api/product";
 
 export default function OrderPage() {
-  // state
-  const [isModeAdmin, setIsModeAdmin] = useState(true)
-  const [isCollapsed, setIsCollapsed] = useState(false)
-  const [currentTabSelected, setCurrentTabSelected] = useState("add")
-  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
-  const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT)
-  const titleEditRef = useRef()
-  const { menu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu()
-  const { basket, handleAddToBasket, handleDeleteBasketProduct } = useBasket()
-  const { username } = useParams()
+	// state
+	const [isModeAdmin, setIsModeAdmin] = useState(true);
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [currentTabSelected, setCurrentTabSelected] = useState("add");
+	const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+	const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
+	const titleEditRef = useRef();
+	const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu();
+	const { basket, handleAddToBasket, handleDeleteBasketProduct } = useBasket();
+	const { username } = useParams();
 
+	const handleProductSelected = async (idProductClicked) => {
+		const productClickedOn = findObjectById(idProductClicked, menu);
+		await setIsCollapsed(false);
+		await setCurrentTabSelected("edit");
+		await setProductSelected(productClickedOn);
+		titleEditRef.current.focus();
+	};
 
-  const handleProductSelected = async (idProductClicked) => {
-    const productClickedOn = findObjectById(idProductClicked, menu)
-    await setIsCollapsed(false)
-    await setCurrentTabSelected("edit")
-    await setProductSelected(productClickedOn)
-    titleEditRef.current.focus()
-  }
+  const initialiseMenu = async() => { 
+    const menuReceived = await getMenu(username);
+    console.log("menuReceived >>", menuReceived);
+    setMenu(menuReceived);
+   }
+  
+	useEffect(() => {
+    initialiseMenu();
+	}, []);
 
-  const orderContextValue = {
-    username,
-    isModeAdmin,
-    setIsModeAdmin,
-    isCollapsed,
-    setIsCollapsed,
-    currentTabSelected,
-    setCurrentTabSelected,
-    menu,
-    handleAdd,
-    handleDelete,
-    resetMenu,
-    newProduct,
-    setNewProduct,
-    productSelected,
-    setProductSelected,
-    handleEdit,
-    titleEditRef,
-    basket,
-    handleAddToBasket,
-    handleDeleteBasketProduct,
-    handleProductSelected,
-  }
+	const orderContextValue = {
+		username,
+		isModeAdmin,
+		setIsModeAdmin,
+		isCollapsed,
+		setIsCollapsed,
+		currentTabSelected,
+		setCurrentTabSelected,
+		menu,
+		handleAdd,
+		handleDelete,
+		resetMenu,
+		newProduct,
+		setNewProduct,
+		productSelected,
+		setProductSelected,
+		handleEdit,
+		titleEditRef,
+		basket,
+		handleAddToBasket,
+		handleDeleteBasketProduct,
+		handleProductSelected,
+	};
 
-  //affichage
-  return (
-    <OrderContext.Provider value={orderContextValue}>
-      <OrderPageStyled>
-        <div className="container">
-          <Navbar />
-          <Main />
-        </div>
-      </OrderPageStyled>
-    </OrderContext.Provider>
-  )
+	//affichage
+	return (
+		<OrderContext.Provider value={orderContextValue}>
+			<OrderPageStyled>
+				<div className="container">
+					<Navbar />
+					<Main />
+				</div>
+			</OrderPageStyled>
+		</OrderContext.Provider>
+	);
 }
 
 const OrderPageStyled = styled.div`
-  background: ${theme.colors.primary};
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	background: ${theme.colors.primary};
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-  .container {
-    background: red;
-    height: 95vh;
-    width: 1400px;
-    display: flex;
-    flex-direction: column;
-    border-radius: ${theme.borderRadius.extraRound};
-  }
-`
+	.container {
+		background: red;
+		height: 95vh;
+		width: 1400px;
+		display: flex;
+		flex-direction: column;
+		border-radius: ${theme.borderRadius.extraRound};
+	}
+`;

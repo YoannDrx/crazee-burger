84ee0106e5ca91490b19130957745e0d75f99152
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-07-15 15:48:34 +0200

    ♻️ F15 : FORM > make slects inputs dynamic and connected to firestore successfully

--- src/components/reusable-ui/SelectInput.js
+++ src/components/reusable-ui/SelectInput.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-export default function SelectInput({options, name, className, id}) {
+export default function SelectInput({value, options, name, className, id, onChange}) {
 	return (
-		<SelectInputStyled name={name} className={className} id={id}>
+		<SelectInputStyled value={value} name={name} className={className} id={id} onChange={onChange}>
           {options.map(({ value, label }) => (
-            <option key={value} value={value}>
+            <option key={label} value={value}>
               {label}
             </option>
           ))}
